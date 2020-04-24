2886477f807591a65e9bef421d36c6dd98f4ac16
CCLancaster <connieclancaster@gmail.com>
2020-04-24 15:37:01 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Spectral', // <title>
+  manifestName: 'Spectral',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Connie Lancaster',
+  subHeading: 'Full time Web Developer. Part time gardener.  ',
+  // social
+  socialLinks: [
+    {
+      style: 'brands',
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/CCLancaster',
+    },
+    {
+      style: 'brands',
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/cclancaster/',
+    },
+    {
+      style: 'solid',
+      icon: 'fa-envelope',
+      name: 'Email',
+      url: 'mailto:connieclancaster@gmail.com',
+    },
+  ],
+};
