dc44fd17c9038c8e9ececbf80509cacb8a43d0f5
CCLancaster <connieclancaster@gmail.com>
2020-04-27 18:15:33 -0700

    edited Readme, ready to launch(part II)

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Connie Lancaster',
   subHeading: 'Full time Web Developer. Part time gardener.  ',
   // social
